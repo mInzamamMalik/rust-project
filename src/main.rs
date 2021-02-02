7688fe52819430aa71bd61171013f52e3d8c412c
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2021-02-02 22:44:09 +0500

    remainig code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 fn main() {
-    println!("Hello, world! piaic");
-    // some comment
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    // let a:&str = "pakistn";
+
+    // let b:String = String::from("pakistan");
+    
+    let std1 = User{
+        username: String::from("pakistan"),
+        email: String::from("sdsdf"),
+        sign_in_count: 34,
+        active: false
+    };
+
+   
+
+
+
+
+
 }
+
+
